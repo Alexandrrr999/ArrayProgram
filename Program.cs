4fb72cd9aa72c77266b303dc2e6eb16dd5573d5b
John Doe <Alexandrrr999@mail.ru>
2022-01-26 18:05:52 +0500

    Add Count element in array

--- Program.cs
+++ Program.cs
@@ -5,3 +5,14 @@
         Console.Write($" {Print[i]} ");
     }
 }
+
+int ThreeArray (string[] Three) // Cчитаем кол-во элементов в массиве, которые <= 3
+{
+    int count = 0;
+    for (int i = 0; i < Three.Length; i++)
+    {
+        if (Three[i].Length <= 3);
+        count++;
+    }
+    return count;
+}
